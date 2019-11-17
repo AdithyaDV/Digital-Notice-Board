40c7c8a9953c26815aaf97e9680112fc0ae3e9fb
adithya149 <adithya.dv.149@gmail.com>
2019-11-17 21:59:11 +0530

    Commit -2

--- EISProject/settings.py
+++ EISProject/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATICFILESDIR   = [
+STATICFILES_DIRS   = [
     os.path.join(BASE_DIR, 'EISProject/static/')
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
